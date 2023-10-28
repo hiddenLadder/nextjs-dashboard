ae49e096d6c0137387fde48641ab139e11819f2f
hiddenLadder <kirill4ernega@gmail.com>
2023-10-28 22:28:01 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-'use client'
 import AcmeLogo from '@/app/ui/acme-logo';
 
 import Link from 'next/link';
 import {lusitana} from "@/app/ui/fonts";
 import Image from "next/image";
 
+export const metadata = {
+    title: 'Главная страница'
+}
 
 export default function Page() {
 
